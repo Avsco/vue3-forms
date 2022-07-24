6894e274b42ec0c90bed7a70fc55d54c7ab2b19e
Alan Benavides <bruno@coastt.org>
2022-07-24 12:36:55 -0400

    add: Base Input vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import BaseInput from './components/BaseInput.vue';
+
+const event = ref({});
+</script>
 
 <template>
-  <div></div>
+  <h1>Vue form</h1>
+  <div>
+    <h2>Name & describe your event</h2>
+    <BaseInput id="title" label="Title" v-model="event.title" />
+    <BaseInput id="description" label="Description" v-model="event.description" />
+
+    <h2>Where is your event?</h2>
+    <BaseInput id="location" label="Location" v-model="event.location" />
+  </div>
+
+  <pre style="text-align: left">
+  {{ event }}
+  </pre>
 </template>
 
 <style scoped></style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineEmits(['update:modelValue']);
+
+defineProps({
+  id: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  typeInput: {
+    type: String,
+    default: 'text',
+  },
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      v-bind="$attrs"
+      class="field"
+      :name="id"
+      :type="typeInput"
+      :placeholder="label"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
